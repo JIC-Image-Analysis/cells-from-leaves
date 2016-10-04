ac779748be84c1fc8ebf9246c5d9b821c32e37f2
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-10-04 11:41:23 +0100

    Simplified the analysis script

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -26,22 +26,18 @@ def analyse_file(fpath, output_directory):
     image = identity(image)
 
 
-def analyse_directory(input_directory, output_directory):
-    """Analyse all the files in a directory."""
-    logging.info("Analysing files in directory: {}".format(input_directory))
-    for fname in os.listdir(input_directory):
-        fpath = os.path.join(input_directory, fname)
-        analyse_file(fpath, output_directory)
-
-
 def main():
     # Parse the command line arguments.
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("input_source", help="Input file/directory")
+    parser.add_argument("input_source", help="Input file")
     parser.add_argument("output_dir", help="Output directory")
     parser.add_argument("--debug", default=False, action="store_true",
                         help="Write out intermediate images")
     args = parser.parse_args()
+
+    # Check that the input file exists.
+    if not os.path.isfile(args.input_source):
+        parser.error("{} not a file".format(args.input_source))
 
     # Create the output directory if it does not exist.
     if not os.path.isdir(args.output_dir):
@@ -65,12 +61,7 @@ def main():
     logging.info("Script version: {}".format(__version__))
 
     # Run the analysis.
-    if os.path.isfile(args.input_source):
-        analyse_file(args.input_source, args.output_dir)
-    elif os.path.isdir(args.input_source):
-        analyse_directory(args.input_source, args.output_dir)
-    else:
-        parser.error("{} not a file or directory".format(args.input_source))
+    analyse_file(args.input_source, args.output_dir)
 
 if __name__ == "__main__":
     main()
