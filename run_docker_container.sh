085a49fd7803b0c9a46a764affda7fce7129cf45
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-11-02 13:11:48 +0000

    Updated run_docker_container.sh to mount paramters directory

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -2,4 +2,4 @@
 
 CONTAINER="cells-from-leaves"
 touch `pwd`/bash_history
-docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v `pwd`/data:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
+docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v `pwd`/data:/data:ro -v `pwd`/parameters:/parameters:ro  -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
