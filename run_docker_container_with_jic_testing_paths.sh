785394a017c77429e0c3a033220baac9071b41c4
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2017-07-13 10:14:00 +0100

    Add bash script for starting docker container to find parameters

---
+++ run_docker_container_with_jic_testing_paths.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CONTAINER="cells-from-leaves"
+INPUT_DIR="/usr/users/JIC_a5/olssont/group_data/scicomp/incoming/mansfiec/2017-05-18-analysis-begins/data"
+touch `pwd`/bash_history
+docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v $INPUT_DIR:/data:ro -v `pwd`/parameters:/parameters:ro  -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
